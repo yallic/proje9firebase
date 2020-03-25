133cb5377538c35354a5354320c0eb39de31e90e
yallic <fatihyallic@gmail.com>
2020-03-26 00:34:39 +0300

    "sign out eklendi "

--- app/src/main/java/com/yallic/proje9firebase/FeedActivity.java
+++ app/src/main/java/com/yallic/proje9firebase/FeedActivity.java
@@ -1,14 +1,53 @@
 package com.yallic.proje9firebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class FeedActivity extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.insta_options_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.upload){
+
+            Intent intent = new Intent(FeedActivity.this,UploadActivity.class);
+            startActivity(intent);
+
+        }else if (item.getItemId()==R.id.signout){
+
+            firebaseAuth.signOut();
+
+            Intent intent = new Intent(FeedActivity.this,SignInActivity.class);
+            startActivity(intent);
+            finish();
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+
+        firebaseAuth = FirebaseAuth.getInstance();
     }
 }
